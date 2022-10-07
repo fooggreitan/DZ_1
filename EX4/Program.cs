89aa46a2a185bb20d8072dd7a854180c191459eb
LEN4 <fooggreitan@gmail.com>
2022-10-07 18:20:13 +0300

    4  Work

---
+++ EX4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите любое число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int counter = -numbers;
+
+while (counter <= numbers) 
+{
+    if(counter % 2 == 0 & counter > 0)
+    {
+        Console.Write($"{counter}");
+    }
+  
+    counter = counter + 1;
+}
