f2c85c7bdd4ce5bdfd144fdc7ad2a4c4c648e002
LEN4 <fooggreitan@gmail.com>
2022-10-07 16:53:03 +0300

    1  Work

---
+++ EX1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число первое: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число второе: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if (a < b) 
+{
+    Console.WriteLine($"Max число: {b}");
+    Console.WriteLine($"Min число: {a}");
+}
+
+else
+{
+    Console.WriteLine($"Max число: {a}");
+    Console.WriteLine($"Min число: {b}");
+}
+    
