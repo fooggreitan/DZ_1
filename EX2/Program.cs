a8258d396b41fe4631043e91fb9119f2b13ebcf9
LEN4 <fooggreitan@gmail.com>
2022-10-07 17:15:42 +0300

    2 Work

---
+++ EX2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число первое: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число второе: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число третье: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > b && a > c)
+    Console.WriteLine($"Max число: {a}");
+else if (b > c) 
+    Console.WriteLine($"Max число: {b}");
+else
+    Console.WriteLine($"Max число: {c}");
