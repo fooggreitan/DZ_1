e66d4bfd1837edc55fcf53891cb795cdd8cd697d
LEN4 <fooggreitan@gmail.com>
2022-10-07 17:32:10 +0300

    3 Work

---
+++ EX3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+    Console.WriteLine("Число является четным");
+else Console.WriteLine("Число не является четным");
